80f33d5690fa44b94d6860638e21b5f869659907
17MV <palban.markvincent@cec.edu.ph>
2024-11-21 22:33:14 +0800

    committed

--- config/services.php
+++ config/services.php
@@ -35,10 +35,9 @@ return [
         ],
     ],
 
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+   'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID', ''),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET', ''),
+        'redirect' => env('GOOGLE_REDIRECT_URI', 'http://127.0.0.1:8000/auth/google/callback'),
     ],
-
 ];
